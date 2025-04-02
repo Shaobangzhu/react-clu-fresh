d562ff066fc1d10b3da375545ee77693b3dd8d57
Chaoran Lu <andyluloveusc@gmail.com>
2025-04-01 18:05:34 -0700

    add code snippet in index.tsx to dynamically adjust the root font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import 'normalize.css';
 import './styles/app.css';
 
 function App() {
   return (
-    <div className="page">
-      <img src={require('.')} />
+    <div className="page guide-page">
+      <img 
+        className='main-pic' 
+        src={require('./Assets/clufresh_logo_icon_@2x.png')} 
+      />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import 'normalize.css';
-import './styles/border.css';
+import "./styles/border.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
+import App from "./App";
+
+// Dynamically adjust the root font size based on the viewport width to support responsive scaling (e.g., for REM units)
+window.addEventListener("resize", () => {
+  document.documentElement.style.fontSize =
+    (document.documentElement.clientWidth / 375) * 100 + "px";
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
